55a38c58fb291cae5ce1b0586a754781d59bedd7
Luke Youngblood <luke@moonwell.fi>
2023-07-30 09:49:58 -0700

    Updates Base Goerli contracts

--- config/basegoerli.config.ts
+++ config/basegoerli.config.ts
@@ -1,9 +1,9 @@
 import { Config } from './config-types'
 
 const config = new Config(
-  '0xa1939Fcf6aA77a68a1f55Ff759c8Fe6a0eC59020',
+  '0xD73f191a50D4BFb5301AE0dF27F5164332df4618',
   'ETH',
-  '0xc71Ed236b67576cf43e2Eb1c961559f31213404b',
+  '0xdd702A5B463adB3b8AfddDB77B88DFaca8B2D3Ca',
   '0x0000000000000000000000000000000000000000',
   2147483647, // no protocol native pair, start at max i32 (never)
   0,
